14880be9654eb798b9840b68b584955e25e15801
vnascimento001 <vnascimento001@hotmail.com>
2024-03-05 06:28:39 -0300

    Refatoração funcionario

--- src/main/java/br/com/avaliacao/action/FuncionarioAction.java
+++ src/main/java/br/com/avaliacao/action/FuncionarioAction.java
@@ -11,115 +11,120 @@ import jakarta.ejb.EJB;
 
 @EJB
 public class FuncionarioAction extends ActionSupport {
-	    private List<Funcionario> funcionarios;
-	    private int pageNumber;
-	    private int pageSize = 10;
-	    private String nmFuncionario;
-	    private int cdFuncionario;
-	    
-	    public String listarFuncionarios() {
-	        if (pageNumber <= 0) {
-	            pageNumber = 1;
-	        }
-	        FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-	        funcionarios = funcionarioDAO.listarFuncionarios(pageNumber, pageSize);
-	        
-	        // Calcular previousPageNumber e nextPageNumber
-	        int previousPageNumber = Math.max(1, pageNumber - 1);
-	        int nextPageNumber = pageNumber + 1;
-	        
-	        // Verificar se a próxima página é válida
-	        if (nextPageNumber > getTotalPages()) {
-	            nextPageNumber = getTotalPages(); // Redirecionar para a última página válida
-	        }
-	        // Adicionar os valores calculados ao contexto do JSP
-	        ActionContext.getContext().put("previousPageNumber", previousPageNumber);
-	        ActionContext.getContext().put("nextPageNumber", nextPageNumber);
-	        return SUCCESS;
-	    }
-	    
-	    public String adicionarFuncionario() {
-	        return SUCCESS;
-	    }
-	    
-	    public String salvarFuncionario() {
-	    	if (nmFuncionario.isBlank()){
-	    		return ERROR;
-	    	}
-	        FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-	        Funcionario funcionario = new Funcionario();
-	        funcionario.setNmFuncionario(nmFuncionario);
-	        funcionarioDAO.inserirFuncionario(funcionario);
-	        return SUCCESS;
-	    }
-	    
-	    public String editarFuncionario() {
-	    	return SUCCESS;
-	        }
-	    
-	    public String atualizarFuncionario() {
-	    	if (nmFuncionario.isBlank()){
-	    		return INPUT;
-	    	}
-	        FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-	        Funcionario funcionario = new Funcionario();
-	        funcionario.setCdFuncionario(cdFuncionario);
-	        funcionario.setNmFuncionario(nmFuncionario);
-	        funcionarioDAO.atualizarFuncionario(funcionario);
-	        return SUCCESS;
-	    }
-	    
-	    public String deletarFuncionario() {
-	    	try {
-		        FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-		        funcionarioDAO.deletarFuncionario(cdFuncionario);
-		        return SUCCESS;
-	    	}catch (Exception e){
-	    		return ERROR;
-	    	}
-	    }
-	    
-	    public int getTotalPages() {
-	        FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-	        int totalRecords = funcionarioDAO.countTotalFuncionarios();
-	        return (int) Math.ceil((double) totalRecords / pageSize);
-	    }
-	    
-	    public List<Funcionario> getFuncionarios() {
-	        return funcionarios;
-	    }
-	    
-	    public int getCdFuncionario() {
-	        return cdFuncionario;
-	    }
+    private List<Funcionario> funcionarios;
+    private int pageNumber;
+    private int pageSize = 10;
+    private String nmFuncionario;
+    private int cdFuncionario;
 
-	    public void setCdFuncionario(int cdFuncionario) {
-	        this.cdFuncionario = cdFuncionario;
-	    }
-	    
-	    public String getNmFuncionario() {
-	        return nmFuncionario;
-	    }
+    private FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
 
-	    public void setNmFuncionario(String nmFuncionario) {
-	        this.nmFuncionario = nmFuncionario;
-	    }
-	    
-	    
-	    public void setPageNumber(int pageNumber) {
-	        this.pageNumber = pageNumber;
-	    }
+    public FuncionarioAction() {
+    }
 
-	    public void setPageSize(int pageSize) {
-	        this.pageSize = pageSize;
-	    }
-	    
-	    public int getPageNumber() {
-	        return pageNumber;
-	    }
+    public FuncionarioAction(FuncionarioDAO funcionarioDAO) {
+        this.funcionarioDAO = funcionarioDAO;
+    }
 
-	    public int getPageSize() {
-	        return pageSize;
-	    }  
+    public String listarFuncionarios() {
+        if (pageNumber <= 0) {
+            pageNumber = 1;
+        }
+        funcionarios = funcionarioDAO.listarFuncionarios(pageNumber, pageSize);
+        calcularPaginas();
+        return SUCCESS;
+    }
 
+    public String adicionarFuncionario() {
+        return SUCCESS;
+    }
+
+    public String salvarFuncionario() {
+        if (validarNomeFuncionario()) {
+            Funcionario funcionario = new Funcionario();
+            funcionario.setNmFuncionario(nmFuncionario);
+            funcionarioDAO.inserirFuncionario(funcionario);
+            return SUCCESS;
+        }
+        return ERROR;
+    }
+
+    public String editarFuncionario() {
+        return SUCCESS;
+    }
+
+    public String atualizarFuncionario() {
+        if (validarNomeFuncionario()) {
+            Funcionario funcionario = new Funcionario();
+            funcionario.setCdFuncionario(cdFuncionario);
+            funcionario.setNmFuncionario(nmFuncionario);
+            funcionarioDAO.atualizarFuncionario(funcionario);
+            return SUCCESS;
+        }
+        return INPUT;
+    }
+
+    public String deletarFuncionario() {
+        try {
+            funcionarioDAO.deletarFuncionario(cdFuncionario);
+            return SUCCESS;
+        } catch (Exception e) {
+            return ERROR;
+        }
+    }
+
+    private void calcularPaginas() {
+        int previousPageNumber = Math.max(1, pageNumber - 1);
+        int nextPageNumber = pageNumber + 1;
+        if (nextPageNumber > getTotalPages()) {
+            nextPageNumber = getTotalPages();
+        }
+        ActionContext.getContext().put("previousPageNumber", previousPageNumber);
+        ActionContext.getContext().put("nextPageNumber", nextPageNumber);
+    }
+    
+    public int getTotalPages() {
+        int totalRecords = funcionarioDAO.countTotalFuncionarios();
+        return (int) Math.ceil((double) totalRecords / pageSize);
+    }
+    
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    private boolean validarNomeFuncionario() {
+        return nmFuncionario != null && !nmFuncionario.isBlank();
+    }
+    
+    public int getCdFuncionario() {
+        return cdFuncionario;
+    }
+
+    public void setCdFuncionario(int cdFuncionario) {
+        this.cdFuncionario = cdFuncionario;
+    }
+
+    public String getNmFuncionario() {
+        return nmFuncionario;
+    }
+
+    public void setNmFuncionario(String nmFuncionario) {
+        this.nmFuncionario = nmFuncionario;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageNumber(int pageNumber) {
+        return pageNumber;
+    }
+
+    public int getPageSize(int pageSize) {
+        return pageSize;
+    }
 }
--- src/main/java/br/com/avaliacao/dao/FuncionarioDAO.java
+++ src/main/java/br/com/avaliacao/dao/FuncionarioDAO.java
@@ -12,44 +12,6 @@ import jakarta.ejb.*;
 import br.com.avaliacao.model.Funcionario;
 @Stateless
 public class FuncionarioDAO {
-	
-	
-    public List<Funcionario> listarFuncionarios(int pageNumber, int pageSize) {
-        List<Funcionario> funcionarios = new ArrayList<>();
-        try (Connection conn = Conexao.getConnection();
-             PreparedStatement ps = conn.prepareStatement("SELECT cd_funcionario, nm_funcionario FROM funcionario LIMIT ? OFFSET ?")) {
-            ps.setInt(1, pageSize);
-            ps.setInt(2, (pageNumber - 1) * pageSize);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                Funcionario funcionario = new Funcionario();
-                funcionario.setCdFuncionario(rs.getInt("cd_funcionario"));
-                funcionario.setNmFuncionario(rs.getString("nm_funcionario"));
-                funcionarios.add(funcionario);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return funcionarios;
-    }
-
-    public List<Funcionario> buscarFuncionariosAtivos() {
-    	List<Funcionario> funcionarios = new ArrayList<>();
-        String sql = "SELECT cd_funcionario, nm_funcionario FROM funcionario";
-        try (Connection conn = Conexao.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-            	Funcionario funcionario = new Funcionario();
-            	funcionario.setCdFuncionario(rs.getInt("cd_funcionario"));
-            	funcionario.setNmFuncionario(rs.getString("nm_funcionario"));
-            	funcionarios.add(funcionario);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return funcionarios;
-    }
 
     @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public void inserirFuncionario(Funcionario funcionario) {
@@ -59,7 +21,7 @@ public class FuncionarioDAO {
             ps.setString(1, funcionario.getNmFuncionario());
             ps.executeUpdate();
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Erro ao inserir funcionário", e);
         }
     }
 
@@ -72,7 +34,7 @@ public class FuncionarioDAO {
             ps.setInt(2, funcionario.getCdFuncionario());
             ps.executeUpdate();
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Erro ao atualizar funcionário", e);
         }
     }
 
@@ -84,10 +46,39 @@ public class FuncionarioDAO {
             ps.setInt(1, cdFuncionario);
             ps.executeUpdate();
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Erro ao deletar funcionário", e);
         }
     }
-    
+
+    public List<Funcionario> listarFuncionarios(int pageNumber, int pageSize) {
+        return listarFuncionariosBase("SELECT cd_funcionario, nm_funcionario FROM funcionario LIMIT ? OFFSET ?", pageNumber, pageSize);
+    }
+
+    public List<Funcionario> buscarFuncionariosAtivos() {
+        return listarFuncionariosBase("SELECT cd_funcionario, nm_funcionario FROM funcionario", 1, Integer.MAX_VALUE);
+    }
+
+    private List<Funcionario> listarFuncionariosBase(String sql, int pageNumber, int pageSize) {
+        List<Funcionario> funcionarios = new ArrayList<>();
+        try (Connection conn = Conexao.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            if (sql.contains("LIMIT")) {
+                ps.setInt(1, pageSize);
+                ps.setInt(2, (pageNumber - 1) * pageSize);
+            }
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Funcionario funcionario = new Funcionario();
+                funcionario.setCdFuncionario(rs.getInt("cd_funcionario"));
+                funcionario.setNmFuncionario(rs.getString("nm_funcionario"));
+                funcionarios.add(funcionario);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao listar funcionários", e);
+        }
+        return funcionarios;
+    }
+
     public int countTotalFuncionarios() {
         int totalRecords = 0;
         try (Connection conn = Conexao.getConnection();
@@ -97,7 +88,7 @@ public class FuncionarioDAO {
                 totalRecords = rs.getInt(1);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Erro ao contar total de funcionários", e);
         }
         return totalRecords;
     }
